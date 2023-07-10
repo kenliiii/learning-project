92f479faf634094eaf368a57cdf53df4a3feb3c1
kenliiii <138658770+kenliiii@users.noreply.github.com>
2023-07-10 18:56:06 +0800

    ken

--- newpach/src/App.js
+++ newpach/src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useDispatch, useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useEffect } from "react";
+import { cartAction } from "./store/cart-slice";
+import {collection,getDoc} from 'firebase/firestore'
+import { db } from "./FireStore";
+let isstart=true
 function App() {
+  const isShowCart = useSelector((state) => state.ui.cartIsShow);
+  const cart = useSelector((state) => state.cart);
+  const dispatch=useDispatch()
+  const useCollertionRef=collection(db,'carts')
+
+
+  useEffect(() => {
+    const sendHttp = async (cart) => {
+      const response = await fetch(
+        "https://react-http-35cf1-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        { method: "put", body: JSON.stringify(cart) }
+      );
+      if (!response.ok) {
+        throw Error("can not send");
+      }
+   
+    };
+if(isstart){
+  isstart=false
+  return
+}  
+    try {
+      sendHttp(cart);
+    } catch {
+      console.log("error");
+    }}
+  , [cart]);
+  
+  useEffect(()=>{
+    const getData=async()=>{
+    const sendHttpGet = async () => {
+      const response = await getDoc(useCollertionRef)
+      
+        if (!response.ok) {
+          throw Error("can not send");
+        }
+      
+       const responseDate=await response.json()
+      
+
+       return responseDate
+      };
+      try {
+        const data= await sendHttpGet();
+        console.log(data);
+      dispatch(cartAction.replace(data))
+      } catch {
+        console.log("error");
+      }
+  }
+  getData()
+},[])
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      {isShowCart && <Cart />}
+      <Products />
+    </Layout>
   );
 }
 
