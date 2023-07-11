bbaced0cf1d3498d4f4335649175c63a35cd3a6f
kenliiii <138658770+kenliiii@users.noreply.github.com>
2023-07-11 12:13:15 +0800

    ken

--- newpach/src/App2.js
+++ newpach/src/App2.js
@@ -1,10 +1,10 @@
 import React, {useContext} from 'react';
 
 import Login from './components/Login/Login';
-import Home from './components/Home/Home';
+
 import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './components/store/auth-context';
-import { AuthContextProvider } from './components/store/auth-context';
+
 import App from "./App";
 function App2() {
   const ctx = useContext(AuthContext)
@@ -15,10 +15,10 @@ function App2() {
       {!ctx.isLoggedIn && <Login/>}
     </main>
   </React.Fragment>
-  return (
-      <AuthContextProvider>
-        {ctx.isLoggedIn ? <App/>:login}
-      </AuthContextProvider>
+  console.log(ctx.isLoggedIn)
+  return (<React.Fragment>
+      {ctx.isLoggedIn ? <App/>:login}
+      </React.Fragment>
   )
 }
 
--- newpach/src/index.js
+++ newpach/src/index.js
@@ -2,12 +2,14 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import './index.css';
-
+import { AuthContextProvider } from './components/store/auth-context';
 
 import store from './store';
 import App2 from "./App2";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Provider store={store}>
+<AuthContextProvider>
     <App2/>
+</AuthContextProvider>
 </Provider>);
