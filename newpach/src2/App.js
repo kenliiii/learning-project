1256db51cb89c6835282c4703cc4a2d126b90647
kenliiii <138658770+kenliiii@users.noreply.github.com>
2023-07-11 11:44:33 +0800

    ken

--- newpach/src2/App.js
+++ newpach/src2/App.js
@@ -4,7 +4,7 @@ import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 import AuthContext from './components/store/auth-context';
-
+import { AuthContextProvider } from './components/store/auth-context';
 function App() {
   const ctx = useContext(AuthContext)
   return (
