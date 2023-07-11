e5b3efd66cb61a3599448690dc26d086cdd82593
kenliiii <138658770+kenliiii@users.noreply.github.com>
2023-07-11 12:19:38 +0800

    kengfag

--- newpach/src/components/Layout/MainHeader.js
+++ newpach/src/components/Layout/MainHeader.js
@@ -1,8 +1,10 @@
 import CartButton from '../Cart/CartButton';
 import classes from './MainHeader.module.css';
+import React, {useContext} from "react";
+import AuthContext from "../store/auth-context";
 
 const MainHeader = (props) => {
-  
+    const ctx = useContext(AuthContext)
   return (
     <header className={classes.header}>
       <h1>ReduxCart</h1>
@@ -12,7 +14,9 @@ const MainHeader = (props) => {
             <CartButton />
           </li>
             <li>
-
+                <button>
+                    <button onClick={ctx.onLogout}>Logout</button>
+                </button>
             </li>
         </ul>
       </nav>
