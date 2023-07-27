c5718b8ccb7b09f112a44169e8688b04668acfc7
kenliiii <138658770+kenliiii@users.noreply.github.com>
2023-07-27 17:32:04 +0800

    ken

--- newpach/src/index.js
+++ newpach/src/index.js
@@ -7,10 +7,12 @@ import './index.css';
 import store from './store';
 import App2 from "./App2";
 import {AuthContextProvider} from "./components/store/auth-context";
+import {Try2} from "./chart";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Provider store={store}>
     <AuthContextProvider>
     <App2/>
+     <Try2/>
     </AuthContextProvider>
 </Provider>);
